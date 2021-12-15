9621cb917887cf1d51654c7923428886b3756039
AdrianIncera <ainceraa01@educantabria.es>
2021-12-15 12:44:05 +0100

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
 
-export default function App() {
+import { Lista } from './components/Lista';
+
+export function App() {
+  const elementos1 = ['Elemento (Comp) 1', 'Elemento (Comp) 2'];
+
   return (
     <div>
-      <h1>Hola,Soy Adrian :)</h1>
-      <p>Este es mi primer proyecto en stackblitz</p>
+      <h1>Hola (desde componente)</h1>
+      <Lista titulo="Componente 1" icono=":(" elementos={elementos1} />
+      <Lista titulo="Componente 2" icono=":*" />
+      <Lista titulo="Componente 3" icono=";)" />
     </div>
   );
 }
